1e74b6f8a9affc42cebdb76bef02a753eff415d9
jiejietop <1161959934@qq.com>
2019-06-26 19:28:14 +0800

    更新queue

--- include/queue.h
+++ include/queue.h
@@ -1,6 +1,19 @@
 #ifndef _QUEUE_H_
 #define _QUEUE_H_
 
+#include "dos_def.h"
+#include <mem.h>
+/* debug */
+#include <debug.h>
+
+struct Dos_Queue
+{
+  dos_void  *Queue_Addr;
+};
+
+typedef struct Dos_Queue * Dos_Queue_t;
+
+
 
 
 
--- src/queue.c
+++ src/queue.c
@@ -1,1 +1,6 @@
 #include <queue.h>
+
+
+
+
+
