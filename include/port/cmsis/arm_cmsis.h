aefc8679b6fb83b5227b367a2f8785570731bc9c
jiejietop <1161959934@qq.com>
2019-07-09 17:54:35 +0800

    更新arm_cmsis.h文件

--- include/port/cmsis/arm_cmsis.h
+++ include/port/cmsis/arm_cmsis.h
@@ -33,34 +33,10 @@
 #endif
 
 
-///**
-//  \brief   Get Control Register
-//  \details Returns the content of the Control Register.
-//  \return               Control Register value
-// */
-//__STATIC_INLINE uint32_t __get_CONTROL(void)
-//{
-//  register uint32_t __regControl         __ASM("control");
-//  return(__regControl);
-//}
-
-
-///**
-//  \brief   Set Control Register
-//  \details Writes the given value to the Control Register.
-//  \param [in]    control  Control Register value to set
-// */
-//__STATIC_INLINE void __set_CONTROL(uint32_t control)
-//{
-//  register uint32_t __regControl         __ASM("control");
-//  __regControl = control;
-//}
-
-
 /**
-  \brief   Get IPSR Register
-  \details Returns the content of the IPSR Register.
-  \return               IPSR Register value
+ * Get IPSR Register 
+ * Returns the content of the IPSR Register.
+ * return IPSR Register value
  */
 __STATIC_INLINE uint32_t __get_IPSR(void)
 {
@@ -70,9 +46,9 @@ __STATIC_INLINE uint32_t __get_IPSR(void)
 
 
 /**
-  \brief   Get APSR Register
-  \details Returns the content of the APSR Register.
-  \return               APSR Register value
+ * Get APSR Register
+ * Returns the content of the APSR Register.
+ * return APSR Register value
  */
 __STATIC_INLINE uint32_t __get_APSR(void)
 {
@@ -82,9 +58,9 @@ __STATIC_INLINE uint32_t __get_APSR(void)
 
 
 /**
-  \brief   Get xPSR Register
-  \details Returns the content of the xPSR Register.
-  \return               xPSR Register value
+ * Get xPSR Register
+ * Returns the content of the xPSR Register.
+ * return xPSR Register value
  */
 __STATIC_INLINE uint32_t __get_xPSR(void)
 {
@@ -94,9 +70,9 @@ __STATIC_INLINE uint32_t __get_xPSR(void)
 
 
 /**
-  \brief   Get Process Stack Pointer
-  \details Returns the current value of the Process Stack Pointer (PSP).
-  \return               PSP Register value
+ * Get Process Stack Pointer
+ * Returns the current value of the Process Stack Pointer (PSP).
+ * return PSP Register value
  */
 __STATIC_INLINE uint32_t __get_PSP(void)
 {
@@ -106,9 +82,9 @@ __STATIC_INLINE uint32_t __get_PSP(void)
 
 
 /**
-  \brief   Set Process Stack Pointer
-  \details Assigns the given value to the Process Stack Pointer (PSP).
-  \param [in]    topOfProcStack  Process Stack Pointer value to set
+ * Set Process Stack Pointer
+ * Assigns the given value to the Process Stack Pointer (PSP).
+ * @param [in]  topOfProcStack  Process Stack Pointer value to set
  */
 __STATIC_INLINE void __set_PSP(uint32_t topOfProcStack)
 {
@@ -118,9 +94,9 @@ __STATIC_INLINE void __set_PSP(uint32_t topOfProcStack)
 
 
 /**
-  \brief   Get Main Stack Pointer
-  \details Returns the current value of the Main Stack Pointer (MSP).
-  \return               MSP Register value
+ * Get Main Stack Pointer
+ * Returns the current value of the Main Stack Pointer (MSP).
+ * return MSP Register value
  */
 __STATIC_INLINE uint32_t __get_MSP(void)
 {
@@ -130,39 +106,15 @@ __STATIC_INLINE uint32_t __get_MSP(void)
 
 
 /**
-  \brief   Set Main Stack Pointer
-  \details Assigns the given value to the Main Stack Pointer (MSP).
-  \param [in]    topOfMainStack  Main Stack Pointer value to set
+ * Set Main Stack Pointer
+ * Assigns the given value to the Main Stack Pointer (MSP).
+ * @param [in] topOfMainStack  Main Stack Pointer value to set
  */
 __STATIC_INLINE void __set_MSP(uint32_t topOfMainStack)
 {
   register uint32_t __regMainStackPointer     __ASM("msp");
   __regMainStackPointer = topOfMainStack;
 }
-
-
-///**
-//  \brief   Get Priority Mask
-//  \details Returns the current state of the priority mask bit from the Priority Mask Register.
-//  \return               Priority Mask value
-// */
-//__STATIC_INLINE uint32_t __get_PRIMASK(void)
-//{
-//  register uint32_t __regPriMask         __ASM("primask");
-//  return(__regPriMask);
-//}
-
-
-///**
-//  \brief   Set Priority Mask
-//  \details Assigns the given value to the Priority Mask Register.
-//  \param [in]    priMask  Priority Mask
-// */
-//__STATIC_INLINE void __set_PRIMASK(uint32_t priMask)
-//{
-//  register uint32_t __regPriMask         __ASM("primask");
-//  __regPriMask = (priMask);
-//}
 
 
 #endif  /**_ARM_CMSIS_H_ */
