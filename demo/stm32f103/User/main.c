95010b01a5365c07f087965728ac83ad18de84ee
jiejietop <1161959934@qq.com>
2019-07-11 22:35:05 +0800

    相关测试改动的文件

--- demo/stm32f103/User/main.c
+++ demo/stm32f103/User/main.c
@@ -12,6 +12,7 @@
 #include "queue.h"
 #include <sem.h>
 #include <mutex.h>
+#include <event.h>
 
 /**
   ******************************************************************
@@ -28,7 +29,7 @@ DOS_TaskCB_t task1 = DOS_NULL;
 Dos_Queue_t queue = DOS_NULL;
 Dos_Sem_t sem = DOS_NULL;
 Dos_Mutex_t mutex = DOS_NULL;
-
+Dos_Event_t event = DOS_NULL;
 /**
   ******************************************************************
 														函数声明
@@ -41,9 +42,11 @@ void test_task(void *Parameter)
 {
   dos_err ret;
 //  dos_uint8 buff[20];
+//  Dos_TaskSleep(1800);
+////  Dos_TaskSleep(30);
+//  Dos_EventWait(event, 1, WAIT_ANY_EVENT, DOS_WAIT_FOREVER - 1);
   
-  Dos_TaskSleep(30);
-//  
+//  printf("task event end\n");
 ////  printf("task sem delete\n");
 ////  Dos_SemDelete(sem);
 ////  sem = DOS_NULL;
@@ -60,41 +63,41 @@ void test_task(void *Parameter)
 //  Dos_QueueRead(queue,buff,10,5500);
 //  
 //  printf("task queue end\n");
-  printf("task sem start\n");
-  Dos_SemWait(sem, DOS_WAIT_FOREVER);
-  printf("task sem end\n");
+//  printf("task sem start\n");
+//  Dos_SemWait(sem, DOS_WAIT_FOREVER);
+//  printf("task sem end\n");
   while(1)
   {
-    Dos_Interrupt_Disable();
-    printf("task pend mutex\n");
-    Dos_Interrupt_Enable(0);
-    ret = Dos_MutexPend(mutex, DOS_WAIT_FOREVER);
-    Dos_Interrupt_Disable();
-    if(ret == DOS_OK)
-    {
-      printf("task runing\n");
-    }
-    else
-    {
-      printf("task err\n");
-    }
-    
 //    Dos_Interrupt_Disable();
-//    DOS_PRINT_DEBUG("ABC\n");
+    printf("task pend mutex\n");
 //    Dos_Interrupt_Enable(0);
-//    Delay_ms(1000);
-    
-    printf("task post mutex\n");
-    Dos_Interrupt_Enable(0);
-    ret = Dos_MutexPost(mutex);
-    if(ret != DOS_OK)
-    {
-      Dos_Interrupt_Disable();
-      printf("task err\n");
-      Dos_Interrupt_Enable(0);
-    }
-    
-    Dos_TaskSleep(2000);
+//    ret = Dos_MutexPend(mutex, DOS_WAIT_FOREVER);
+//    Dos_Interrupt_Disable();
+//    if(ret == DOS_OK)
+//    {
+//      printf("task runing\n");
+//    }
+//    else
+//    {
+//      printf("task err\n");
+//    }
+//    
+////    Dos_Interrupt_Disable();
+////    DOS_PRINT_DEBUG("ABC\n");
+////    Dos_Interrupt_Enable(0);
+////    Delay_ms(1000);
+//    
+//    printf("task post mutex\n");
+//    Dos_Interrupt_Enable(0);
+//    ret = Dos_MutexPost(mutex);
+//    if(ret != DOS_OK)
+//    {
+//      Dos_Interrupt_Disable();
+//      printf("task err\n");
+//      Dos_Interrupt_Enable(0);
+//    }
+//    
+    Dos_TaskSleep(3000);
     
   }
 }
@@ -102,8 +105,11 @@ void test1_task(void *Parameter)
 {
 //  dos_uint8 buff[20] = "ASFASF";
 //  
-  Dos_TaskSleep(1800);
-//  
+//  Dos_TaskSleep(1800);
+  
+//  Dos_EventWait(event, 2, WAIT_ANY_EVENT, 5000);
+  
+  Dos_EventSet(event, 5);
 //  printf("task1 write queue1\n");
 //  
 ////  Dos_TaskDelete(task);
@@ -115,28 +121,31 @@ void test1_task(void *Parameter)
 //  Dos_QueueWrite(queue,buff,10,0);
 
 //  printf("task1 write sem\n");
-  printf("task1 sem start\n");
-  Dos_SemPost(sem);
-  Dos_SemPost(sem);
-  printf("task1 sem end\n");
+//  printf("task1 sem start\n");
+//  Dos_SemPost(sem);
+//  Dos_SemPost(sem);
+//  printf("task1 sem end\n");
 //  Dos_SemWait(sem, DOS_WAIT_FOREVER);
   while(1)
   {
-    Dos_Interrupt_Disable();
-//    DOS_PRINT_DEBUG("123\n");
+//    Dos_Interrupt_Disable();
+////    DOS_PRINT_DEBUG("123\n");
     printf("task1 runing\n");
-    Dos_Interrupt_Enable(0);
-//    Delay_ms(1000);
-    Dos_TaskSleep(100);
+//    Dos_Interrupt_Enable(0);
+////    Delay_ms(1000);
+    Dos_TaskSleep(2000);
   }
 }
 void test2_task(void *Parameter)
 {
   dos_err ret;
-  static uint32_t i;
-  printf("task2 sem start\n");
-  Dos_SemWait(sem, DOS_WAIT_FOREVER);
-  printf("task2 sem end\n");
+//  Dos_TaskSleep(1800);
+//  Dos_EventWait(event, 4, WAIT_ANY_EVENT, DOS_WAIT_FOREVER);
+  
+//  static uint32_t i;
+//  printf("task2 event end\n");
+//  Dos_SemWait(sem, DOS_WAIT_FOREVER);
+//  printf("task2 sem end\n");
 //  dos_uint8 buff[20];
 //  
 //  Dos_TaskSleep(1000);
@@ -151,41 +160,41 @@ void test2_task(void *Parameter)
 //  printf("task2 queue start1\n");
   while(1)
   {
-    Dos_Interrupt_Disable();
-    printf("task2 pend mutex\n");
-    Dos_Interrupt_Enable(0);
-    ret = Dos_MutexPend(mutex, DOS_WAIT_FOREVER);
-    Dos_Interrupt_Disable();
-    if(ret == DOS_OK)
-    {
-      printf("task2 runing\n");
-    }
-    else
-    {
-      printf("task2 err\n");
-    }
-    Dos_Interrupt_Enable(0);
 //    Dos_Interrupt_Disable();
-//    DOS_PRINT_DEBUG("456\n");
+    printf("task2 pend mutex\n");
 //    Dos_Interrupt_Enable(0);
-//    Delay_ms(1000);
-    
-    for(i=0;i<2000000;i++)
-		{
-			DOS_TASK_YIELD();
-		}
-    
-    Dos_Interrupt_Disable();
-    printf("task2 post mutex\n");
-    Dos_Interrupt_Enable(0);
-    
-    ret = Dos_MutexPost(mutex);
-    if(ret != DOS_OK)
-    {
-      Dos_Interrupt_Disable();
-      printf("task2 err\n");
-      Dos_Interrupt_Enable(0);
-    }
+//    ret = Dos_MutexPend(mutex, DOS_WAIT_FOREVER);
+//    Dos_Interrupt_Disable();
+//    if(ret == DOS_OK)
+//    {
+//      printf("task2 runing\n");
+//    }
+//    else
+//    {
+//      printf("task2 err\n");
+//    }
+//    Dos_Interrupt_Enable(0);
+////    Dos_Interrupt_Disable();
+////    DOS_PRINT_DEBUG("456\n");
+////    Dos_Interrupt_Enable(0);
+////    Delay_ms(1000);
+//    
+//    for(i=0;i<2000000;i++)
+//		{
+//			DOS_TASK_YIELD();
+//		}
+//    
+//    Dos_Interrupt_Disable();
+//    printf("task2 post mutex\n");
+//    Dos_Interrupt_Enable(0);
+//    
+//    ret = Dos_MutexPost(mutex);
+//    if(ret != DOS_OK)
+//    {
+//      Dos_Interrupt_Disable();
+//      printf("task2 err\n");
+//      Dos_Interrupt_Enable(0);
+//    }
 
     Dos_TaskSleep(1000);
   }
@@ -214,9 +223,7 @@ int main(void)
   queue = Dos_QueueCreate(10,10);
   sem = Dos_BinarySem_Create(0);
   mutex = Dos_MutexCreate();
-  
-//  Dos_MutexDelete(mutex);
-////  mutex = DOS_NULL;
+  event = Dos_EventCreate();
   
   task = Dos_TaskCreate( "task",
                   &test_task,
