1f59ddeccb420772b6c45c5da7af76075fd4f1e0
jiejietop <1161959934@qq.com>
2019-07-11 22:31:16 +0800

    完善事件event模块(已通过测试)

--- include/event.h
+++ include/event.h
@@ -1,7 +1,15 @@
 #ifndef _EVENT_H_
 #define _EVENT_H_
+#include <dos_def.h>
+#include <mem.h>
+#include <list.h>
+#include <task.h>
 
 
+#define     WAIT_EVENT_OP       0x03        /** waiting event options mask */
+#define     WAIT_ANY_EVENT      0x01        /** waiting any event */
+#define     WAIT_ALL_EVENT      0x02        /** waiting all event */
+#define     NO_CLR_EVENT        0x04        /** do not clear events */
 
 struct Dos_Event
 {
@@ -11,6 +19,10 @@ struct Dos_Event
 typedef struct Dos_Event * Dos_Event_t;
 
 
-
+Dos_Event_t Dos_EventCreate(void);
+dos_err Dos_EventDelete(Dos_Event_t event);
+dos_uint32 Dos_EventWait(Dos_Event_t event, dos_uint32 wait_event, dos_uint32 op, dos_uint32 timeout);
+dos_uint32 Dos_EventSet(Dos_Event_t event, dos_uint32 set_event);
 
 #endif // !_EVENT_H_
+
---
+++ src/event.c
@@ -0,0 +1,197 @@
+#include <event.h>
+#include <mem.h>
+#include <string.h>
+#include <dos_config.h>
+#include <task.h>
+#include <port.h>
+
+
+static dos_bool _Dos_CheckEvent(Dos_Event_t event, dos_uint32 want_event, dos_uint32 op)
+{
+    dos_uint32 option;
+
+    option = op & WAIT_EVENT_OP;
+
+    switch (option)
+    {
+        case WAIT_ANY_EVENT:
+            if((event->EventSet & want_event) != 0)
+                goto got;
+            break;
+
+        case WAIT_ALL_EVENT:
+            if((event->EventSet & want_event) == want_event)
+                goto got;
+            break;
+
+        default:
+            break;
+    }
+
+    return DOS_FALSE;
+got:
+    if(!(op & NO_CLR_EVENT))
+    {
+        event->EventSet &= ~(want_event);
+        
+    }
+    return DOS_TRUE;
+}
+
+/**
+ * create a event set
+ */
+Dos_Event_t Dos_EventCreate(void)
+{
+    Dos_Event_t event;
+
+    event = (Dos_Event_t)Dos_MemAlloc(sizeof(struct Dos_Event));
+    if(event == DOS_NULL)
+    {
+        DOS_PRINT_DEBUG("event is null\n");
+        return DOS_NULL;
+    }
+    
+    event->EventSet = 0;
+    Dos_TaskList_Init(&(event->EventPend));
+
+    return event;
+}
+
+
+/**
+ * delete a event set
+ * description: You need to set the event pointer to null after deleting the event set
+ */
+dos_err Dos_EventDelete(Dos_Event_t event)
+{
+    if(event != DOS_NULL)
+    {
+        if(Dos_TaskList_IsEmpty(&(event->EventPend)))
+        {
+            memset(event,0,sizeof(struct Dos_Event));
+            Dos_MemFree(event);
+            return DOS_OK;
+        }
+        else
+        {
+            DOS_PRINT_DEBUG("there are tasks in the event pend list\n");
+            return DOS_NOK;
+        }
+    }
+    else
+    {
+        DOS_PRINT_DEBUG("event is null\n");
+        return DOS_NOK;
+    }
+}
+
+/**
+ * event wait function
+ */
+dos_uint32 Dos_EventWait(Dos_Event_t event, dos_uint32 wait_event, dos_uint32 op, dos_uint32 timeout)
+{
+    DOS_TaskCB_t task;
+
+    if((event == DOS_NULL) ||(wait_event == 0))
+    {
+        DOS_PRINT_DEBUG("event is null or the waiting event is 0\n");
+        return 0;
+    }
+
+    if((op & WAIT_EVENT_OP) == WAIT_EVENT_OP)
+    {
+        DOS_PRINT_DEBUG("waiting for event option is invalid\n");
+        return 0;
+    }
+    
+    if(_Dos_CheckEvent(event, wait_event, op) == DOS_TRUE)
+    {
+        return wait_event;  /** waiting for the event to succeed */
+    }
+
+    if((timeout == 0) || (Dos_Scheduler_IsLock()))  /** scheduler is lock */
+    {
+        return 0;
+    }
+
+    if(Dos_ContextIsInt())
+    {
+        DOS_PRINT_ERR("event wait time is not 0, and the context is in an interrupt\n");
+        return 0;
+    }
+
+    task = (DOS_TaskCB_t)Dos_Get_CurrentTCB();
+    task->WaitEvent = wait_event;
+    task->WaitEventOp = op;
+
+    Dos_TaskWait(&event->EventPend, timeout);
+
+    /** Task resumes running */
+    if(task->TaskStatus & DOS_TASK_STATUS_TIMEOUT)
+    {
+        DOS_RESET_TASK_STATUS(task, (DOS_TASK_STATUS_TIMEOUT | DOS_TASK_STATUS_SUSPEND));
+        DOS_SET_TASK_STATUS(task, DOS_TASK_STATUS_READY);
+        Dos_TaskItem_Del(&(task->PendItem));
+        DOS_PRINT_DEBUG("waiting for event timeout\n");
+    }
+
+    if(_Dos_CheckEvent(event, wait_event, op) == DOS_TRUE)
+    {
+        return wait_event;  /** waiting for the event to succeed */
+    }
+
+    return 0;
+}
+
+/**
+ * set a event set
+ */
+dos_uint32 Dos_EventSet(Dos_Event_t event, dos_uint32 set_event)
+{
+    dos_uint32 value;
+    DOS_TaskCB_t task;
+
+    if((event == DOS_NULL) ||(set_event == 0))
+    {
+        DOS_PRINT_DEBUG("event is null or the setting event is 0\n");
+        return 0;
+    }
+
+    event->EventSet |= set_event;
+
+    if(!(Dos_TaskList_IsEmpty(&event->EventPend)))
+    {
+        Dos_Scheduler_Lock();
+
+        for(value = Dos_Get_TaskListValue(&event->EventPend); value > 0; value--)
+        {
+            task = Dos_Get_NextTCB(&event->EventPend);
+
+            if(_Dos_CheckEvent(event, task->WaitEvent, task->WaitEventOp) == DOS_TRUE)
+            {
+                Dos_TaskWake(task);
+            }
+        }
+
+        Dos_Scheduler_Unlock();
+    }
+
+  return event->EventSet;
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
