f86933a99e83977ac8570897c57606e7ada1581c
jiejietop <1161959934@qq.com>
2019-07-15 21:44:37 +0800

    更新配置文件，添加软件定时器任务相关宏定义

--- include/config/dos_config.h
+++ include/config/dos_config.h
@@ -9,22 +9,27 @@
 
 #include "stm32f10x.h"
 
-#define         DOS_MAX_PRIORITY_NUM            32U
+#define         DOS_MAX_PRIORITY_NUM            (32U)
 
 #define         DOS_LOGO                        DOS_LOGO_BIG    /** Choose big logo or small logo */
 
 #define         DOS_IDLE_TASK_PRIORITY          (DOS_MAX_PRIORITY_NUM - 1U)
-#define         DOS_IDLE_TASK_SIZE              128U
+#define         DOS_IDLE_TASK_SIZE              (128U)
+#define         DOS_IDLE_TASK_TICK              (20U)
+
+#define         DOS_SWTMR_TASK_PRIORITY         (0U)
+#define         DOS_SWTMR_TASK_SIZE             (512U)
+#define         DOS_SWTMR_TASK_TICK             (20U)
 
 #define         DOS_SYSTEM_CLOCK_HZ             (SystemCoreClock)
-#define         DOS_SYSTICK_CLOCK_HZ            1000U
+#define         DOS_SYSTICK_CLOCK_HZ            (1000U)
 
 
-#define         DOS_ALIGN_SIZE                  (8)
+#define         DOS_ALIGN_SIZE                  (8U)
 
-#define         MEM_HEAP_SIZE                   1024*30
+#define         MEM_HEAP_SIZE                   (1024*30)
 
-#define         DOS_IPC_QUEUQ                   1
+#define         DOS_IPC_QUEUQ                   (1U)
 
 
 #endif
